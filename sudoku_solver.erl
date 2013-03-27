4d535a8ce4c82fffaf2880729f8b467e73536f7e
genesislive <genesislive@126.com>
2013-03-27 17:20:05 +0800

    convert tab to space.

--- sudoku_solver.erl
+++ sudoku_solver.erl
@@ -1,6 +1,6 @@
 %%%----------------------------------------------------------------
 %%% @author Genesislive <genesislive@126.com>
-%%% 	[https://]
+%%%     [https://]
 %%% @copyright 2013 
 %%% @doc Erlang Sodoku Solver
 %%% @end
@@ -24,7 +24,7 @@
 %% @end
 %%-----------------------------------------------------------------
 solve(Puzzle) when is_list(Puzzle) ->
-	dfs(list_to_dict(Puzzle)).
+    dfs(list_to_dict(Puzzle)).
 
 %%%================================================================
 %%% Internal functions
@@ -36,11 +36,11 @@ solve(Puzzle) when is_list(Puzzle) ->
 %% @end
 %%-----------------------------------------------------------------
 list_to_dict(Puzzle) ->
-	% element
-	element(2, lists:foldl(fun(Elm, {Num, Dict}) ->
-				    {Num + 1, dict:store({Num div 9, Num rem 9}, Elm, Dict)} % dict:store创建一个新dict 
-				end, {0, dict:new()}, Puzzle)).
-	
+    % element
+    element(2, lists:foldl(fun(Elm, {Num, Dict}) ->
+                    {Num + 1, dict:store({Num div 9, Num rem 9}, Elm, Dict)} % dict:store创建一个新dict 
+                end, {0, dict:new()}, Puzzle)).
+    
 %%-----------------------------------------------------------------
 %% @doc check if Val is valid for the Nth element of Puzzle
 %% @spec check(Puzzle::) -> ok.
@@ -49,42 +49,42 @@ list_to_dict(Puzzle) ->
 %% @end
 %%-----------------------------------------------------------------
 print(Puzzle) ->
-	% io:format("~p~n", [dict:to_list(Puzzle)]),
-	lists:foreach(fun(X) ->
-					 lists:foreach(fun(Y) ->
-									  io:format("~p ", [dict:fetch({X, Y}, Puzzle)])
-								   end, lists:seq(0, 8)),
-					 io:format("~n", [])
-				  end, lists:seq(0, 8)).
-	
+    % io:format("~p~n", [dict:to_list(Puzzle)]),
+    lists:foreach(fun(X) ->
+                     lists:foreach(fun(Y) ->
+                                      io:format("~p ", [dict:fetch({X, Y}, Puzzle)])
+                                   end, lists:seq(0, 8)),
+                     io:format("~n", [])
+                  end, lists:seq(0, 8)).
+    
 %%-----------------------------------------------------------------
 %% @doc check if Val is valid for the Nth element of Puzzle
 %% @spec check(N::integer(), Val::integer(), Puzzle::dict()) -> true | false
 %% @end
 %%-----------------------------------------------------------------
 check(N, Val, Puzzle) ->
-	case lists:all(fun(Row) ->			% 检查N所在行是否合法
-				 	  dict:fetch({Row, N rem 9}, Puzzle) /= Val
-			       end, lists:seq(0, 8)) of
-		false -> false;
-		true ->
-			case lists:all(fun(Col) ->			% 检查N所在列是否合法
-							  dict:fetch({N div 9, Col}, Puzzle) /= Val
-						   end, lists:seq(0, 8)) of
-				false -> false;
-				true ->
-					Row = N div 9 div 3 * 3,		
-					Col = N rem 9 div 3 * 3,		
-					case lists:all(fun(X) ->		% 检查格子N所在小九宫是否合法
-									  lists:all(fun(Y) ->
-									  	           dict:fetch({X, Y}, Puzzle) /= Val
-									  		    end, lists:seq(Col, Col + 2))
-								   end, lists:seq(Row, Row + 2)) of
-						true -> true;
-						false -> false
-					end
-			end
-	end.
+    case lists:all(fun(Row) ->          % 检查N所在行是否合法
+                      dict:fetch({Row, N rem 9}, Puzzle) /= Val
+                   end, lists:seq(0, 8)) of
+        false -> false;
+        true ->
+            case lists:all(fun(Col) ->          % 检查N所在列是否合法
+                              dict:fetch({N div 9, Col}, Puzzle) /= Val
+                           end, lists:seq(0, 8)) of
+                false -> false;
+                true ->
+                    Row = N div 9 div 3 * 3,        
+                    Col = N rem 9 div 3 * 3,        
+                    case lists:all(fun(X) ->        % 检查格子N所在小九宫是否合法
+                                      lists:all(fun(Y) ->
+                                                   dict:fetch({X, Y}, Puzzle) /= Val
+                                                end, lists:seq(Col, Col + 2))
+                                   end, lists:seq(Row, Row + 2)) of
+                        true -> true;
+                        false -> false
+                    end
+            end
+    end.
 
 %%-----------------------------------------------------------------
 %% @doc solve sudoku using Depth-First-Search
@@ -92,23 +92,23 @@ check(N, Val, Puzzle) ->
 %% @end
 %%-----------------------------------------------------------------
 dfs(Puzzle) ->
-	dfs(0, Puzzle).
+    dfs(0, Puzzle).
 
 dfs(N, Puzzle) when N > 80 ->
-	io:format("Solution found.~n"),
-	print(Puzzle),
-	io:format("~n");
+    io:format("Solution found.~n"),
+    print(Puzzle),
+    io:format("~n");
 dfs(N, Puzzle) ->
-	Val = dict:fetch({N div 9, N rem 9}, Puzzle),
-	if
-		Val /= 0 -> % 当前位不为空时跳过
-			dfs(N + 1, Puzzle);
-		Val == 0 -> % enum all possible value
-			lists:foreach(fun(Num) ->
-							 case check(N, Num, Puzzle) of
-							 	true ->
-							 		dfs(N + 1, dict:store({N div 9, N rem 9}, Num, Puzzle));
-							 	false -> failed
-							 end
-						  end, lists:seq(1, 9))
-	end.
+    Val = dict:fetch({N div 9, N rem 9}, Puzzle),
+    if
+        Val /= 0 -> % 当前位不为空时跳过
+            dfs(N + 1, Puzzle);
+        Val == 0 -> % enum all possible value
+            lists:foreach(fun(Num) ->
+                             case check(N, Num, Puzzle) of
+                                true ->
+                                    dfs(N + 1, dict:store({N div 9, N rem 9}, Num, Puzzle));
+                                false -> failed
+                             end
+                          end, lists:seq(1, 9))
+    end.
